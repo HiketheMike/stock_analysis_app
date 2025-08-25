7acc78462689c6742a85ef29c38b347e7467260e
Hikethemike <pandavunguyen@gmail.com>
2025-08-25 18:37:25 +0700

    12

--- stock_analysis_app.py
+++ stock_analysis_app.py
@@ -13,7 +13,8 @@ import requests
 def get_sp500_components():
     sp500_url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    df = requests.get(sp500_url, headers)
+    response = requests.get(sp500_url, headers=headers)
+    df = pd.read_html(response.text)
     df = df[0]
     tickers = df['Symbol']
     tickers_companies_dict = dict(zip(df['Symbol'], df['Security']))
